621b41669759dbd04ca8ccf1670f630cddee6f8e
Jamie Durman <81934222+jamied889@users.noreply.github.com.>
2026-03-28 19:16:35 -0500

    Spring Boot Neon Ark API

--- neon-ark-intake-tracker/my-docker-java-monster/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Monster m = new Monster("Vexxar", "Void");
-
-        System.out.println("Your monster has been created.");
-        System.out.println("Description: " + m.getDescription());
-    }
-}
